cd2c66edeee3dfedc748035f6dfbf0071e0aaea4
Arthur Beaulieu <contact@abeaulieu.fr>
2020-11-13 19:05:05 +0100

    add theming first css var

--- web/js/App.js
+++ web/js/App.js
@@ -1,35 +1,36 @@
 'use strict';
 
-var LF440 = new window.LightFever440();
+const AJAX = new Kom();
+const LF440 = new window.LightFever440();
 
-document.querySelector('.js-btn-start').addEventListener('click', () => {
-  LF440.ajax({
-    type: 'POST',
-    url: 'action',
-    data: {
-      action: 'ON'
-    },
-    success: (response) => {
-      console.log('Strip led successfuly started');
-    },
-    error: (status) => {
-      console.warn('Something went wrong', status);
-    }
-  });
-});
-
-document.querySelector('.js-btn-stop').addEventListener('click', () => {
-  LF440.ajax({
-    type: 'POST',
-    url: 'action',
-    data: {
-      action: 'OFF'
-    },
-    success: (response) => {
-      console.log('Strip led successfuly stoped');
-    },
-    error: (status) => {
-      console.warn('Something went wrong', status);
-    }
-  });
-});
+// document.querySelector('.js-btn-start').addEventListener('click', () => {
+//   LF440.ajax({
+//     type: 'POST',
+//     url: 'action',
+//     data: {
+//       action: 'ON'
+//     },
+//     success: (response) => {
+//       console.log('Strip led successfuly started');
+//     },
+//     error: (status) => {
+//       console.warn('Something went wrong', status);
+//     }
+//   });
+// });
+//
+// document.querySelector('.js-btn-stop').addEventListener('click', () => {
+//   LF440.ajax({
+//     type: 'POST',
+//     url: 'action',
+//     data: {
+//       action: 'OFF'
+//     },
+//     success: (response) => {
+//       console.log('Strip led successfuly stoped');
+//     },
+//     error: (status) => {
+//       console.warn('Something went wrong', status);
+//     }
+//   });
+// });
--- web/js/LightFever440.js
+++ web/js/LightFever440.js
@@ -2,36 +2,54 @@ class LightFever440 {
 
 
   constructor() {
+    document.getElementById('btn-on').addEventListener('click', () => {
+      AJAX.post('action', {
+        action: 'ON'
+      }).then(response => {
+        console.log('Strip led successfuly started');
+      }).catch(error => {
+        console.warn('Something went wrong', status);
+      });
+    });
 
+    document.getElementById('btn-off').addEventListener('click', () => {
+      AJAX.post('action', {
+        action: 'OFF'
+      }).then(response => {
+        console.log('Strip led successfuly stoped');
+      }).catch(error => {
+        console.warn('Something went wrong', status);
+      });
+    });
   }
 
 
-  ajax(options) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (this.readyState === 4 && (this.status === 200 || this.status === 204)) {
-        if (options.hasOwnProperty('success') && typeof options.success === 'function') {
-          const response = (this.responseText) ? JSON.parse(this.responseText) : null;
-          options.success(response);
-        }
-      } else if (this.readyState === 4 && (this.status !== 200 || this.status !==  204)) {
-        if (options.hasOwnProperty('error') && typeof options.error === 'function') {
-          options.error(this.status);
-        }
-      }
-    };
-
-    const type = options.type.toUpperCase();
-    xhr.open(type, options.url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('Accept', 'application/json');
-
-    if (type === 'POST') {
-      xhr.send(JSON.stringify(options.data));
-    } else {
-      xhr.send();
-    }
-  }
+  // ajax(options) {
+  //   let xhr = new XMLHttpRequest();
+  //   xhr.onreadystatechange = function() {
+  //     if (this.readyState === 4 && (this.status === 200 || this.status === 204)) {
+  //       if (options.hasOwnProperty('success') && typeof options.success === 'function') {
+  //         const response = (this.responseText) ? JSON.parse(this.responseText) : null;
+  //         options.success(response);
+  //       }
+  //     } else if (this.readyState === 4 && (this.status !== 200 || this.status !==  204)) {
+  //       if (options.hasOwnProperty('error') && typeof options.error === 'function') {
+  //         options.error(this.status);
+  //       }
+  //     }
+  //   };
+  //
+  //   const type = options.type.toUpperCase();
+  //   xhr.open(type, options.url, true);
+  //   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  //   xhr.setRequestHeader('Accept', 'application/json');
+  //
+  //   if (type === 'POST') {
+  //     xhr.send(JSON.stringify(options.data));
+  //   } else {
+  //     xhr.send();
+  //   }
+  // }
 
 
 }
