66b5311c51ef6387c016c90f17526145dd7f9343
ArthurBeaulieu <contact@abeaulieu.fr>
2020-11-18 16:27:54 +0100

    optionnal color in analyzer mode

--- web/js/components/AnalyzerController.js
+++ web/js/components/AnalyzerController.js
@@ -11,13 +11,15 @@ class AnalyzerController {
         container: document.getElementById('auto-uniform-options'),
         peakDetection: document.getElementById('auto-uniform-peak-detection'),
         peakSensitivity: document.getElementById('auto-uniform-peak-sensitivity'),
-        peakSensitivityText: document.getElementById('auto-uniform-peak-sensitivity-value')
+        peakSensitivityText: document.getElementById('auto-uniform-peak-sensitivity-value'),
+        colorSwitch: document.getElementById('auto-uniform-color-switch'),
+        color: document.getElementById('auto-uniform-color')
       },
       PROGRESSIVE: {
         button: document.getElementById('auto-progressive'),
         container: document.getElementById('auto-progressive-options'),
-        lightNumber: document.getElementById('auto-progressive-size'),
-        lightNumberText: document.getElementById('auto-progressive-size-value'),
+        size: document.getElementById('auto-progressive-size'),
+        sizeText: document.getElementById('auto-progressive-size-value'),
         reverse: document.getElementById('auto-progressive-reverse')
       },
       PULSE: {
@@ -25,6 +27,7 @@ class AnalyzerController {
         container: document.getElementById('auto-pulse-options'),
         maxLength: document.getElementById('auto-pulse-length'),
         maxLengthText: document.getElementById('auto-pulse-length-value'),
+        colorSwitch: document.getElementById('auto-pulse-color-switch'),
         color: document.getElementById('auto-pulse-color')
       }
     };
@@ -40,25 +43,50 @@ class AnalyzerController {
     this._dom.PROGRESSIVE.button.addEventListener('click', this._updateEffect.bind(this));
     this._dom.PULSE.button.addEventListener('click', this._updateEffect.bind(this));
 
+    this._dom.UNIFORM.colorSwitch.addEventListener('input', () => {
+      window.localStorage.setItem('auto-uniform-color-switch', this._dom.UNIFORM.colorSwitch.checked);
+      if (this._dom.UNIFORM.colorSwitch.checked) {
+        this._dom.UNIFORM.color.parentNode.style.filter = 'opacity(1)';
+      } else {
+        this._dom.UNIFORM.color.parentNode.style.filter = 'opacity(0.1)';
+      }
+    });
+    this._dom.UNIFORM.color.addEventListener('click', event => {
+      event.preventDefault();
+      if (this._dom.UNIFORM.colorSwitch.checked === true) {
+        Utils.colorPickerModal(window.localStorage.getItem('auto-uniform-color'), color => {
+          window.localStorage.setItem('auto-uniform-color', color);
+          this._dom.UNIFORM.color.value = color
+        });
+      }
+    });
     this._dom.UNIFORM.peakSensitivity.addEventListener('input', () => {
       this._dom.UNIFORM.peakSensitivityText.innerHTML = this._dom.UNIFORM.peakSensitivity.value;
       window.localStorage.setItem('auto-uniform-peak-sensitivity', this._dom.UNIFORM.peakSensitivity.value);
     });
 
-    this._dom.PROGRESSIVE.lightNumber.addEventListener('input', () => {
-      this._dom.PROGRESSIVE.lightNumberText.innerHTML = this._dom.PROGRESSIVE.lightNumber.value;
-      window.localStorage.setItem('auto-progressive-size', this._dom.PROGRESSIVE.lightNumber.value);
+    this._dom.PROGRESSIVE.size.addEventListener('input', () => {
+      this._dom.PROGRESSIVE.sizeText.innerHTML = this._dom.PROGRESSIVE.size.value;
+      window.localStorage.setItem('auto-progressive-size', this._dom.PROGRESSIVE.size.value);
     });
 
+    this._dom.PULSE.maxLength.addEventListener('input', () => {
+      this._dom.PULSE.maxLengthText.innerHTML = this._dom.PULSE.maxLength.value;
+      window.localStorage.setItem('auto-pulse-length', this._dom.PULSE.maxLength.value);
+    });
+    this._dom.PULSE.colorSwitch.addEventListener('input', () => {
+      window.localStorage.setItem('auto-pulse-color-switch', this._dom.PULSE.colorSwitch.checked);
+      if (this._dom.PULSE.colorSwitch.checked) {
+        this._dom.PULSE.color.parentNode.style.filter = 'opacity(1)';
+      } else {
+        this._dom.PULSE.color.parentNode.style.filter = 'opacity(0.1)';
+      }
+    });
     this._dom.PULSE.color.addEventListener('click', event => {
       event.preventDefault();
       Utils.colorPickerModal('pulse-color', color => {
         this._dom.PULSE.color.value = color
       });
-    });
-    this._dom.PULSE.maxLength.addEventListener('input', () => {
-      this._dom.PULSE.maxLengthText.innerHTML = this._dom.PULSE.maxLength.value;
-      window.localStorage.setItem('auto-pulse-length', this._dom.PULSE.maxLength.value);
     });
 
     this._dom.UNIFORM.peakSensitivity.addEventListener('change', this._updateEffect.bind(this, 'UNIFORM'));
@@ -69,7 +97,7 @@ class AnalyzerController {
   _initState() {
     // Initialize range sliders with saved values
     window.rangesliderJs.create(this._dom.UNIFORM.peakSensitivity, { value: window.localStorage.getItem('auto-uniform-peak-sensitivity') });
-    window.rangesliderJs.create(this._dom.PROGRESSIVE.lightNumber, { value: window.localStorage.getItem('auto-progressive-size') });
+    window.rangesliderJs.create(this._dom.PROGRESSIVE.size, { value: window.localStorage.getItem('auto-progressive-size') });
     window.rangesliderJs.create(this._dom.PULSE.maxLength, { value: window.localStorage.getItem('auto-pulse-length') });
   }
 
@@ -145,16 +173,21 @@ class AnalyzerController {
         peakDetection: this._dom.UNIFORM.peakDetection.checked,
         peakSensitivity: parseInt(this._dom.UNIFORM.peakSensitivity.value) / 100
       };
+      if (this._dom.UNIFORM.colorSwitch.checked) {
+        options.color = Utils.hexToRgb(this._dom.UNIFORM.color.value);
+      }
     } else if (window.LF440.effect === 'PROGRESSIVE') {
       options = {
-        lightNumber: parseInt(this._dom.PROGRESSIVE.lightNumber.value),
+        size: parseInt(this._dom.PROGRESSIVE.size.value),
         reverse: this._dom.PROGRESSIVE.reverse.checked
       };
     } else if (window.LF440.effect === 'PULSE') {
       options = {
-        color: Utils.hexToRgb(this._dom.PULSE.color),
-        maxLength: parseInt(this._dom.PULSE.maxLength.value)
+        max: parseInt(this._dom.PULSE.maxLength.value) / 100
       };
+      if (this._dom.PULSE.colorSwitch.checked) {
+        options.color = Utils.hexToRgb(this._dom.PULSE.color);
+      }
     }
 
     return options;
