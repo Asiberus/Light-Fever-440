cba4625d7a74cd1c5bac7ab5035237deacba34be
ArthurBeaulieu <contact@abeaulieu.fr>
2020-11-19 16:20:48 +0100

    again...

--- web/js/components/InputFactory.js
+++ web/js/components/InputFactory.js
@@ -7,9 +7,6 @@ class InputFactory {
   constructor(options) {
     this._scope = options.scope;
     this._update = options.update;
-    // This flag allow to start a transaction on a slider. When true, the change evt can be fire.
-    // We only want to fire change when the user is actually moving a slider.
-    this._changeEventLock = false;
   }
 
 
@@ -45,29 +42,29 @@ class InputFactory {
 
   _event(verb, options, lsSave, isSwitch = true) {
     options.element.addEventListener(verb, () => {
-      if (verb === 'click' || this._changeEventLock === true) {
-        if (lsSave === true) {
-          if (isSwitch === true) {
-            window.localStorage.setItem(options.lsKey, options.element.checked);
-          } else {
-            window.localStorage.setItem(options.lsKey, options.element.value);
-          }
+      if (lsSave === true) {
+        if (isSwitch === true) {
+          window.localStorage.setItem(options.lsKey, options.element.checked);
+        } else {
+          window.localStorage.setItem(options.lsKey, options.element.value);
         }
-        this._update.call(this._scope, options.effect);
-        this._changeEventLock = false;
       }
+
+      this._update.call(this._scope, options.effect);
     });
   }
 
 
   _createSlider(options) {
     options.label.innerHTML = window.localStorage.getItem(options.lsKey) || options.default;
-    window.rangesliderJs.create(options.element, { value: window.localStorage.getItem(options.lsKey) || options.default });
-    this._event('change', options, true, false);
-    options.element.addEventListener('input', () => {
-      this._changeEventLock = true;
-      window.localStorage.setItem(options.lsKey, options.element.value);
-      options.label.innerHTML = options.element.value;
+    window.rangesliderJs.create(options.element, {
+      value: window.localStorage.getItem(options.lsKey) || options.default,
+      onSlideEnd: (value) => {
+        this._changeEventLock = true;
+        window.localStorage.setItem(options.lsKey, value);
+        options.label.innerHTML = value;
+        this._update.call(this._scope, options.effect);
+      }
     });
   }
 
@@ -93,7 +90,6 @@ class InputFactory {
 
     this._event('change', options, false);
     options.element.addEventListener('input', () => {
-      this._changeEventLock = true;
       window.localStorage.setItem(`${options.lsKey}-switch`, options.element.checked);
       if (options.element.checked === true) {
         options.color.parentNode.style.filter = 'opacity(1)';
@@ -124,7 +120,6 @@ class InputFactory {
 
     this._event('change', options, false);
     options.element.addEventListener('input', () => {
-      this._changeEventLock = true;
       window.localStorage.setItem(`${options.lsKeySwitch}`, options.element.checked);
       if (options.element.checked === false) {
         options.color.parentNode.style.filter = 'opacity(1)';
